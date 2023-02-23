4a955ccd52406f85e38a72eed01b5246a96d2f0f
Aditya <adityaharde05@gmail.com.com>
2023-02-23 18:17:12 +0530

    Fifth

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -42,26 +42,30 @@ public class StudentController {
 
     @GetMapping("/get-student-by-name/{name}")
     public ResponseEntity<Student> getStudentByName(@PathVariable String name){
-        Student student = studentService.findStudentbyName(name); // Assign student by calling service layer method
+        Student student=null;
+        student = studentService.findStudentbyName(name); // Assign student by calling service layer method
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-teacher-by-name/{name}")
     public ResponseEntity<Teacher> getTeacherByName(@PathVariable String name){
-        Teacher teacher = studentService.findTeacherbyName(name); // Assign student by calling service layer method
+        Teacher teacher=null;
+        teacher = studentService.findTeacherbyName(name); // Assign student by calling service layer method
         return new ResponseEntity<>(teacher, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-students-by-teacher-name/{teacher}")
     public ResponseEntity<List<String>> getStudentsByTeacherName(@PathVariable String teacher){
-        List<String> students = studentService.getStudentsbyTeacherName(teacher); // Assign list of student by calling service layer method
+        List<String>students=null;
+        students = studentService.getStudentsbyTeacherName(teacher); // Assign list of student by calling service layer method
 
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
 
     @GetMapping("/get-all-students")
     public ResponseEntity<List<String>> getAllStudents(){
-        List<String> students = studentService.findAllStudents(); // Assign list of student by calling service layer method
+        List<String>students=null;
+        students = studentService.findAllStudents(); // Assign list of student by calling service layer method
 
         return new ResponseEntity<>(students, HttpStatus.CREATED);
     }
